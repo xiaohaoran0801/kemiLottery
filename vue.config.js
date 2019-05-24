d868d95647367c9e905c884dff7a1524db0020d9
xiaohaoran0801 <1256844775@qq.com>
2019-05-24 14:42:05 +0800

    打包优化

---
+++ vue.config.js
@@ -0,0 +1,30 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = ['js', 'css'];
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const isProduction = process.env.NODE_ENV === 'production';
+module.exports = {
+    // publicPath:'./',
+    configureWebpack: config => {
+        if (isProduction) {
+             config.plugins.push(new CompressionWebpackPlugin({
+                algorithm: 'gzip',// 使用gzip压缩
+                test: new RegExp(
+                    '\\.(js|css)$' // 压缩 js 与 css
+                ),
+                threshold: 5000,// 资源文件大于10240B=10kB时会被压缩
+                minRatio: 0.8 // 最小压缩比达到0.8时才会被压缩
+            })),
+            config.plugins.push(new HtmlWebpackPlugin({
+                filename:'index.html',
+                template: './src/index.html'
+            }))
+            //这部分代码就是我们要从bundle中导出来的依赖
+            config.externals = {
+                'vue': 'Vue',
+                'vue-router': 'VueRouter',
+                'element-ui': "ELEMENT",
+                'axios':'axios',
+            }
+        }
+    }
+}
